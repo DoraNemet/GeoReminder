51f7182c48d93e2911c66e5f72e6094f82f1fbc4
Dora Fundak <dora.fundak@gmail.com>
2017-05-14 22:03:38 +0200

    Main layouts

--- app/src/main/java/com/ferit/dfundak/georeminder/MainActivity.java
+++ app/src/main/java/com/ferit/dfundak/georeminder/MainActivity.java
@@ -1,13 +1,27 @@
 package com.ferit.dfundak.georeminder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = (FloatingActionButton) findViewById(R.id.add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddNewItem.class);
+                startActivity(intent);
+            }
+        });
     }
 }
