f3e0c779587f151aa986b523bd13316e3100a0d2
Dora Fundak <dora.fundak@gmail.com>
2017-05-17 09:44:56 +0200

    KV3 more layouts

--- app/src/main/java/com/ferit/dfundak/georeminder/AddNewItem.java
+++ app/src/main/java/com/ferit/dfundak/georeminder/AddNewItem.java
@@ -1,13 +1,57 @@
 package com.ferit.dfundak.georeminder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class AddNewItem extends AppCompatActivity {
+
+    private LinearLayout addTime;
+    private LinearLayout addLocation;
+    private LinearLayout addImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_item);
+
+        addImage = (LinearLayout) findViewById(R.id.add_image);
+        addLocation = (LinearLayout) findViewById(R.id.add_location);
+        addTime = (LinearLayout) findViewById(R.id.add_time);
+
+        addLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermissions();
+                Intent intent = new Intent(AddNewItem.this, LocationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermissions();
+
+            }
+        });
+
+        addTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermissions();
+
+            }
+        });
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    private void checkPermissions() {
+
+
     }
 }
