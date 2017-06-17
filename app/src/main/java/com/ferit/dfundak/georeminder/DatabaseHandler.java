1165d67b1202022b5b7737a56196c90adfbe8686
Dora Fundak <dora.fundak@gmail.com>
2017-06-17 16:54:28 +0200

    database template

---
+++ app/src/main/java/com/ferit/dfundak/georeminder/DatabaseHandler.java
@@ -0,0 +1,32 @@
+package com.ferit.dfundak.georeminder;
+
+/**
+ * Created by Dora on 17/06/2017.
+ */
+/*
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "geoReminder";
+
+    private static final String TABLE_CONTACTS = "reminders";
+
+    // Contacts Table Columns names
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_TITLE = "name";
+    private static final String KEY_DESCRIPTION = "description";
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
+*/
